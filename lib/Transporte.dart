2b5eece400cc38f8221bfbbd650a415bc1b6dc5b
Luis Jose Ferrer Estelles <LJFEREST@upv.edu.es>
2024-04-21 14:20:16 +0200

    creacion pantalla transporte

---
+++ lib/Transporte.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TransporteScreen extends StatelessWidget {
+  const TransporteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(height: 65.0,)
+
+        ],
+      ),
+    );
+  }
+}
