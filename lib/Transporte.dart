aed8ff7215306516354e1a18e3bc917454653633
Luis Jose Ferrer Estelles <LJFEREST@upv.edu.es>
2024-04-24 13:15:47 +0200

    libreria weather

--- lib/Transporte.dart
+++ lib/Transporte.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
+
+
+
+
 
 class TransporteScreen extends StatelessWidget {
   const TransporteScreen({super.key});
 
+  void weather(BuildContext context) async{
+    WeatherFactory wf = new WeatherFactory('1fc6a21d7c1a05d3aff1156d71ff425f', language: Language.SPANISH);
+    Weather w = await wf.currentWeatherByCityName("Valencia");
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +26,7 @@ class TransporteScreen extends StatelessWidget {
             height: 120.0,
             alignment:Alignment.center,
           ),
+
         ],
       ),
     );
