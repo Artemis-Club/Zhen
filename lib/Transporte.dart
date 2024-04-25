4d4151ced7b1deaacaa932348995467108c25a36
Luis Jose Ferrer Estelles <LJFEREST@upv.edu.es>
2024-04-25 15:03:47 +0200

    cambios weather

--- lib/Transporte.dart
+++ lib/Transporte.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 
@@ -8,14 +10,27 @@ import 'package:weather/weather.dart';
 class TransporteScreen extends StatelessWidget {
   const TransporteScreen({super.key});
 
-  Future<String> weather() async{
-    WeatherFactory wf = new WeatherFactory('1fc6a21d7c1a05d3aff1156d71ff425f', language: Language.SPANISH);
-    Weather w = await wf.currentWeatherByCityName("Valencia");
-    return w.toString();
+  Future<String> weather() async {
+    final String apiKey = '1fc6a21d7c1a05d3aff1156d71ff425f';
+    final String cityName = 'Valencia';
+    final String url = 'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&lang=es';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      // Si la respuesta es exitosa, parseamos el JSON
+      final data = jsonDecode(response.body);
+      // Aquí puedes acceder a los datos específicos que necesitas, por ejemplo, la descripción del clima
+      final weatherDescription = data['weather'][0]['description'];
+      return weatherDescription;
+    } else {
+      // Si la respuesta no es exitosa, lanzamos un error
+      throw Exception('Error al obtener el clima: ${response.statusCode}');
+    }
   }
   @override
   Widget build(BuildContext context) {
-    String tiem = weather().toString();
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
@@ -27,7 +42,18 @@ class TransporteScreen extends StatelessWidget {
             height: 120.0,
             alignment:Alignment.center,
           ),
-          Text(tiem),
+          FutureBuilder<String>(
+            future: weather(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(snapshot.data ?? 'No data');
+              }
+            },
+          ),
         ],
       ),
     );
