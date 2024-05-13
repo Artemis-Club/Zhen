797e2b6f3217835a6a5de77ff54f2634c437916b
Luis Jose Ferrer Estelles <LJFEREST@upv.edu.es>
2024-05-13 13:00:17 +0200

    icono tiempo en pantalla 2

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'pointsManager.dart';
 import 'activity_details_dialog.dart';
 import 'ActivityInProgress.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 
 void main() async {
@@ -32,6 +36,21 @@ void main() async {
   await Permission.location.request();
 
   runApp(MyApp(dbService: dbService));
+}
+
+Future<String> weather() async {
+  final response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=Valencia,es&appid=1fc6a21d7c1a05d3aff1156d71ff425f&units=metric&lang=en'));
+
+  if (response.statusCode == 200) {
+    // Si la petición fue exitosa, parseamos el JSON.
+    var data = jsonDecode(response.body);
+    String weatherDescription = data['weather'][0]['description'];
+    //double temperature = data['main']['temp'];
+    return weatherDescription;
+  } else {
+    // Si la petición falló, lanzamos un error.
+    throw Exception('Failed to load weather data');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -452,14 +471,88 @@ class _Screen3State extends State<Screen3> {
                       ),
                     ),
                   ),
-                  SizedBox(width: 20),
+                  SizedBox(width: 10,height: 10),
                   Expanded(
-                    child: Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.indigoAccent,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+                    child: FutureBuilder<String?>(
+                      future: weather(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          // Usa el operador?? para proporcionar un valor predeterminado en caso de que snapshot.data sea nulo.
+                          if(snapshot.data == "few clouds" || snapshot.data == "scattered clouds" || snapshot.data == "broken clouds"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/nublado.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+
+                              ],
+                            );
+                          }
+                          else if(snapshot.data == "shower rain" || snapshot.data == "rain"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/lloviendo.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ],
+                            );
+                          }
+                          else if(snapshot.data == "mist"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/niebla.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ],
+                            );
+                          }
+                          else if(snapshot.data == "snow"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/nievee.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ],
+                            );
+                          }
+                          else if(snapshot.data == "thunderstorm"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/tormenta.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ],
+                            );
+                          }
+                          else if(snapshot.data == "clear sky"){
+                            return Column(
+                              children: [
+                                Image.asset(
+                                  "images/soleado.png",
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ],
+                            );
+                          }
+                          return Text(snapshot.data?? 'Cargando...');
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+
+                        }
+                        // Por defecto, muestra un indicador de carga.
+                        return CircularProgressIndicator();
+                      },
                     ),
                   ),
                 ],
@@ -491,6 +584,7 @@ class _Screen3State extends State<Screen3> {
       ),
     );
   }
+
 
   Widget _buildActivityCard(int index, BuildContext context) {
     List<String> titles = ["BICICLETAS", "TRANSPORTE PUBLICO", "COCHE ELECTRICO", "ANDANDO"];
