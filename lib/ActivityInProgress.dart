046da8405c8bf5628e2947e3f712aa8d7ff73858
roger7titan <arouco16@gmail.com>
2024-05-14 21:11:50 +0200

    first commit

--- lib/ActivityInProgress.dart
+++ lib/ActivityInProgress.dart
@@ -1,53 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
-/*class ActivityInProgress extends StatelessWidget {
-  final String activityName;
-  final bool isKilometers;
-  final Function onStop;
-  final Function onReset;
-
-  ActivityInProgress({
-    required this.activityName,
-    this.isKilometers = false,
-    required this.onStop,
-    required this.onReset,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      decoration: BoxDecoration(
-        color: Color(0xFFFFE4C7),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(activityName,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            Spacer(),
-            IconButton(
-              icon: Icon(CupertinoIcons.check_mark_circled_solid),
-              color: Colors.green,
-              onPressed: () => onStop(),
-            ),
-            IconButton(
-              icon: Icon(CupertinoIcons.xmark_circle_fill),
-              color: Colors.red,
-              onPressed: () => onReset(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
+import 'ActivityTimer.dart';
+/*
 class ActivityInProgress extends StatelessWidget {
   final String activityName;
   final bool isKilometers;
@@ -84,6 +38,55 @@ class ActivityInProgress extends StatelessWidget {
             IconButton(
               icon: const Icon(CupertinoIcons.xmark_circle_fill),
               color: Colors.red,
+              onPressed: () => onReset(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+class ActivityInProgress extends StatelessWidget {
+  final String activityName;
+  final bool isKilometers;
+  final Function onStop;
+  final Function onReset;
+
+  const ActivityInProgress({
+    super.key,
+    required this.activityName,
+    this.isKilometers = false,
+    required this.onStop,
+    required this.onReset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFE4C7),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(activityName, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            Expanded(child: Center(child: ActivityTimer(isKilometers: isKilometers))),  // Pasar isKilometers
+            IconButton(
+              iconSize: 30,
+              icon: const Icon(CupertinoIcons.check_mark_circled_solid),
+              color: Colors.lightGreen,
+              onPressed: () => onStop(),
+            ),
+            IconButton(
+              iconSize: 30,
+              icon: const Icon(CupertinoIcons.xmark_circle_fill),
+              color: Colors.redAccent,
               onPressed: () => onReset(),
             ),
           ],
--- lib/ActivityTimer.dart
+++ lib/ActivityTimer.dart
@@ -1,232 +1,74 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-
-/*class ActivityTimer extends StatefulWidget {
-  final bool isKilometers;
-  final void Function() startTimer; // Callback para manejar la parada desde el exterior
-  final void Function() stopTimer;
-  final void Function() resetTimer;// Callback para manejar el reset desde el exterior
-
-  const ActivityTimer({super.key, this.isKilometers = false, required this.startTimer, required this.stopTimer, required this.resetTimer});
-
-  @override
-  _ActivityTimerState createState() => _ActivityTimerState();
-}
-
-class _ActivityTimerState extends State<ActivityTimer> {
-  Timer? _timer;
-  Duration _duration = const Duration();
-
-  void startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _duration = _duration + const Duration(seconds: 1);
-      });
-    });
-  }
-
-  void stopTimer() {
-    if (_timer!= null && _timer!.isActive) {
-      _timer!.cancel();
-      widget.stopTimer.call(); // Usar el operador?. para evitar errores si onStop es null
-    }
-  }
-
-  void resetTimer() {
-    if (_timer!= null && _timer!.isActive) {
-      _timer!.cancel();
-      widget.resetTimer.call(); // Usar el operador?. para evitar errores si onReset es null
-    }
-    setState(() {
-      _duration = const Duration();
-    });
-  }
-  @override
-  void initState() {
-    super.initState();
-    startTimer();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(_duration.inHours);
-    final minutes = twoDigits(_duration.inMinutes.remainder(60));
-    final seconds = twoDigits(_duration.inSeconds.remainder(60));
-
-    return Text(
-      widget.isKilometers ? '0.0 km' : '$hours:$minutes:$seconds',
-      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-    );
-  }
-}
-*/
-
-
-/*import 'dart:async';
-import 'package:flutter/material.dart';
+import 'pointsManager.dart';
 
 class ActivityTimer extends StatefulWidget {
   final bool isKilometers;
 
-  ActivityTimer({this.isKilometers = false});
-
-  @override
-  _ActivityTimerState createState() => _ActivityTimerState();
-}
-
-class _ActivityTimerState extends State<ActivityTimer> {
-  Timer? _timer;
-  Duration _duration = Duration();
-
-  void startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        _duration = _duration + Duration(seconds: 1);
-      });
-    });
-  }
-
-  void stopTimer() {
-    if (_timer != null && _timer!.isActive) {
-      _timer!.cancel();
-    }
-  }
-
-  void resetTimer() {
-    if (_timer != null && _timer!.isActive) {
-      _timer!.cancel();
-    }
-    setState(() {
-      _duration = Duration();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(_duration.inHours);
-    final minutes = twoDigits(_duration.inMinutes.remainder(60));
-    final seconds = twoDigits(_duration.inSeconds.remainder(60));
-
-    return Text(
-      widget.isKilometers ? '0.0 km' : '$hours:$minutes:$seconds',
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-    );
-  }
-}
-*/
-
-
-class ActivityTimer extends StatefulWidget {
-  final bool isKilometers;
-
-  ActivityTimer({this.isKilometers = false});
+  const ActivityTimer({Key? key, this.isKilometers = false}) : super(key: key);
 
   @override
   ActivityTimerState createState() => ActivityTimerState();
 }
 
-/*class ActivityTimerState extends State<ActivityTimer> {
-  Timer? _timer;
-  Duration _duration = Duration();
-
-  void startTimer() {
-    if (_timer?.isActive != true) {
-      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-        setState(() {
-          _duration = _duration + Duration(seconds: 1);
-        });
-      });
-    }
-  }
-
-  void stopTimer() {
-    if (_timer != null) {
-      _timer!.cancel();
-    }
-  }
-
-  void resetTimer() {
-    stopTimer();
-    setState(() {
-      _duration = Duration();
-    });
-  }
-  void startActivity() {
-    startTimer();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(_duration.inHours);
-    final minutes = twoDigits(_duration.inMinutes.remainder(60));
-    final seconds = twoDigits(_duration.inSeconds.remainder(60));
-
-    return Text(
-      widget.isKilometers ? '0.0 km' : '$hours:$minutes:$seconds',
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-    );
-  }
-}
-*/
 class ActivityTimerState extends State<ActivityTimer> {
   Timer? _timer;
   Duration _duration = Duration();
-
-  void startTimer() {
-    if (_timer?.isActive!= true) {
-      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-        setState(() {
-          _duration = _duration + Duration(seconds: 1);
-        });
-      });
-    }
-  }
-
-  void stopTimer() {
-    if (_timer!= null) {
-      _timer!.cancel();
-    }
-  }
-
-  void resetTimer() {
-    stopTimer();
-    setState(() {
-      _duration = Duration();
-    });
-  }
-
-  void startActivity() {
-    startTimer();
-  }
+  double _kilometers = 0.0;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      startActivity();
+    if (!widget.isKilometers) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (mounted) {
+          startTimer();
+        }
+      });
+    }
+  }
+
+  void startTimer() {
+    if (_timer?.isActive != true) {
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        if (mounted) {
+          setState(() {
+            _duration += Duration(seconds: 1);
+          });
+        }
+      });
+    }
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+  }
+
+  void resetTimer() {
+    stopTimer();
+    setState(() {
+      _duration = Duration();
+      _kilometers = 0.0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(_duration.inHours);
-    final minutes = twoDigits(_duration.inMinutes.remainder(60));
-    final seconds = twoDigits(_duration.inSeconds.remainder(60));
-
-    return Text(
-      widget.isKilometers? '0.0 km' : '$hours:$minutes:$seconds',
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-    );
+    if (widget.isKilometers) {
+      return Text(
+        '${_kilometers.toStringAsFixed(1)} km',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      );
+    } else {
+      String twoDigits(int n) => n.toString().padLeft(2, '0');
+      final hours = twoDigits(_duration.inHours);
+      final minutes = twoDigits(_duration.inMinutes.remainder(60));
+      final seconds = twoDigits(_duration.inSeconds.remainder(60));
+      return Text(
+        '$hours:$minutes:$seconds',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      );
+    }
   }
 }
+
